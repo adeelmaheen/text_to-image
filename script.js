81701b0d628bc62d5547fbd9ff9792abc20127ee
adeelmaheen <157056396+adeelmaheen@users.noreply.github.com>
2024-10-17 16:55:10 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+const key = "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";
+const inputText = document.getElementById("input");
+const image = document.getElementById("image");
+const genBtn = document.getElementById("btn");
+const svg = document.getElementById("svg")
+const load = document.getElementById("loading")
+const ResetBtn = document.getElementById("reset")
+const downloaBtn = document.getElementById("download") 
+
+async function query(data) {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/ZB-Tech/Text-to-Image",
+		{
+			headers: {
+				Authorization: `Bearer ${key}`
+				
+			},
+			method: "POST",
+			body: JSON.stringify({"inputs": inputText.value}),
+		}
+	);
+	const result = await response.blob();
+    image.style.display = "block";
+    load.style.display = "none";
+	return result;
+}
+async function generate() {
+    
+load.style.display = "block";
+query().then((response) => {
+	const objectUrl = URL.createObjectURL(response);
+    image.src = objectUrl;
+    downloaBtn.addEventListener("click",()=>{
+        download(objectUrl)
+    })
+});
+}
+
+genBtn.addEventListener("click", () => {
+    generate();
+    svg.style.display = "none"
+})
+
+inputText.addEventListener("keydown" , (e)=>{
+    if( e.key == "Enter"){
+        generate()
+        svg.style.display = "none"
+    }
+})
+
+ResetBtn.addEventListener("click" , () =>{
+    inputText.value = ""
+    window.location.reload();
+})
+
+function download(objectUrl){
+    fetch(objectUrl).then(res=>res.blob())
+    .then(file =>{
+        let a = document.createElement("a");
+        a.href = URL.createObjectURL(file);
+        a.download = new Date().getTime();
+        a.click();
+
+    })
+    .catch(()=> alert("failed download"));
+}
